ffae2a93cf9b8a80140ef59809248ff27c934491
TDuy22 <phamthanhduyvbhp@gmail.com>
2026-03-09 22:10:43 +0700

    Update base path in Vite configuration to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Ho-Chi-Minh-journey/',
+  base: '/Map_Khang_chien_chong_my/',
 })
